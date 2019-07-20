981bd78f230af8ec24e64311d916cffeee73cbbf
pym7857 <pym7857@naver.com>
2019-07-20 16:59:19 +0900

    Align Overall Design

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: ' "Noto Sans KR" , serif ',
+    }
+})
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
